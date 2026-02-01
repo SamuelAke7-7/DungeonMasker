0a0d5dc81861859b594ff53c05e90b3211b8ece8
Samuel Ake <samuel.ake@segurosimple.com>
2026-02-01 14:08:32 -0600

    Feat: Final details

--- Assets/Scripts/Controllers/EventManagerController.cs
+++ Assets/Scripts/Controllers/EventManagerController.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 public class MapEventManager : MonoBehaviour
 {
     public GameObject panel;
-    public RawImage ghostImage;
+    public GameObject ghostImage;
     public RawImage healedImage;
     public CanvasBlinker canvasBlinker;
     [SerializeField] private TextMeshProUGUI text;
